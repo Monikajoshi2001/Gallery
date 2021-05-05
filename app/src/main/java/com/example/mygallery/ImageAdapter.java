746179e51623bac6870c9b11b308604f5eef7156
Monikajoshi2001 <monikajoshimj1@gmail.com>
2021-05-05 10:33:31 +0530

    complete

--- app/src/main/java/com/example/mygallery/ImageAdapter.java
+++ app/src/main/java/com/example/mygallery/ImageAdapter.java
@@ -1,27 +1,91 @@
 package com.example.mygallery;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
 
 public class ImageAdapter extends BaseAdapter {
+    private Context context;
+    List<String> image_path;
+
+    public ImageAdapter(Context c,List<String> i)
+    {
+        context=c;
+        image_path=i;
+    }
     @Override
     public int getCount() {
-        return 0;
+        if(image_path!=null)
+        {
+            return image_path.size();
+        }
+        else
+            return  0;
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return position;
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        ImageView imageView;
+        BitmapFactory.Options bfOptions=new BitmapFactory.Options();
+        bfOptions.inDither=false;                     //Disable Dithering mode
+        bfOptions.inPurgeable=true;                   //Tell to gc that whether it needs free memory, the Bitmap can be cleared
+        bfOptions.inInputShareable=true;              //Which kind of reference will be used to recover the Bitmap data after being clear, when it will be used in the future
+        bfOptions.inTempStorage=new byte[32 * 1024];
+        if (convertView == null)
+        {
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            imageView.setPadding(0, 0, 0, 0);
+        }
+        else
+         {
+            imageView = (ImageView) convertView;
+         }
+        FileInputStream fs = null;
+        Bitmap bm;
+        try {
+            fs = new FileInputStream(new File(image_path.get(position).toString()));
+            if(fs!=null)
+            {
+                bm=BitmapFactory.decodeFileDescriptor(fs.getFD(), null, bfOptions);
+                imageView.setImageBitmap(bm);
+                imageView.setId(position);
+                imageView.setLayoutParams(new GridView.LayoutParams(200, 160));
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        } finally{
+            if(fs!=null) {
+                try {
+                    fs.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return imageView;
     }
+
 }
--- app/src/main/java/com/example/mygallery/MainActivity.java
+++ app/src/main/java/com/example/mygallery/MainActivity.java
@@ -6,60 +6,125 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    public static final int REQUEST_CODE = 102;
-    public static final int REQUEST_CODE1 = 101;
-    ImageButton camera;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    ImageButton captureBtn = null;
+    final int CAMERA_CAPTURE = 1;
+    private Uri picUri;
+    private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private GridView grid;
+    private  List<String> listOfImagesPath;
+
+    public static final String GridViewDemo_ImagePath =
+            Environment.getExternalStorageDirectory().getAbsolutePath() + "/GridViewDemo/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        camera=(ImageButton) findViewById(R.id.cameraID);
-        camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                askCameraPermission();
-            }
-        });
-    }
+        captureBtn = (ImageButton)findViewById(R.id.cameraID);
+        captureBtn.setOnClickListener(this);
+        grid = ( GridView) findViewById(R.id.grid1);
 
-    private void askCameraPermission() {
-        //checking if the permission is not already given
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
-            //requesting permission
-            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA}, REQUEST_CODE1);
-        }
-        else{
-            openCamera();
+        listOfImagesPath = null;
+        listOfImagesPath = RetriveCapturedImagePath();
+        if(listOfImagesPath!=null){
+            grid.setAdapter(new ImageAdapter(this,listOfImagesPath));
         }
     }
-    //checking the request result
 
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode==101){
-            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
-                openCamera();
-            else{
-                Toast.makeText(MainActivity.this,"Camera permission is Required",Toast.LENGTH_LONG).show();
+    public void onClick(View arg0) {
+        // TODO Auto-generated method stub
+        if (arg0.getId() == R.id.cameraID) {
+
+            try {
+                //use standard intent to capture an image
+                Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //we will handle the returned data in onActivityResult
+                startActivityForResult(captureIntent, CAMERA_CAPTURE);
+            } catch(ActivityNotFoundException anfe){
+                //display an error message
+                String errorMessage = "Whoops – your device doesn’t support capturing images!";
+                Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            //user is returning from capturing an image using the camera
+            if (requestCode == CAMERA_CAPTURE) {
+                Bundle extras = data.getExtras();
+                Bitmap thePic = extras.getParcelable("data");
+                String imgcurTime = dateFormat.format(new Date());
+                File imageDirectory = new File(GridViewDemo_ImagePath);
+                imageDirectory.mkdirs();
+                String _path = GridViewDemo_ImagePath + imgcurTime + ".jpg";
+                try {
+                    FileOutputStream out = new FileOutputStream(_path);
+                    thePic.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                    out.close();
+                } catch (FileNotFoundException e) {
+                    e.getMessage();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                listOfImagesPath = null;
+                listOfImagesPath = RetriveCapturedImagePath();
+                if (listOfImagesPath != null) {
+                    grid.setAdapter(new ImageAdapter(this, listOfImagesPath));
+                }
             }
         }
     }
 
-    private void openCamera()
-    {
-        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(camera, REQUEST_CODE);
+    private List<String> RetriveCapturedImagePath() {
+        List<String> tFileList = new ArrayList<String>();
+        File f = new File(GridViewDemo_ImagePath);
+        if (f.exists()) {
+            File[] files=f.listFiles();
+            Arrays.sort(files);
+            for(int i=0; i<files.length; i++){
+                File file = files[i];
+                if(file.isDirectory())
+                    continue;
+                tFileList.add(file.getPath());
+            }
+        }
+        return tFileList;
     }
 }
